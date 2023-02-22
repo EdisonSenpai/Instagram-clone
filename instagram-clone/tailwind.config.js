6d86433f9a3e6a2c3727a5185343e8b3a3c21c15
Eduard Donea <100487696+EdisonSenpai@users.noreply.github.com>
2023-02-22 16:18:28 +0200

    Adding configs and header

--- instagram-clone/tailwind.config.js
+++ instagram-clone/tailwind.config.js
@@ -5,8 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
